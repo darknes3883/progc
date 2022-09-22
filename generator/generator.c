e98c0c2ef9984f4c1ddd82e2a3c5c2bcce7d920e
ayet merzouqi <MERZOUQIAYET@GMAIL.cOM>
2022-09-22 10:15:58 +0200

    message

---
+++ generator/generator.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "generator.h"
+
+
+//macroconstatntes
+
+#define INITIAL_VALUE 1
+#define INITIAL_STEP 1
+
+//variables privées:environnement permanent du module
+static int init_val=INITIAL_VALUE;
+static int current_val=INITIAL_VALUE;
+static int step=INITIAL_STEP;
+
+
+void generator_define_first(int new_first_value)
+{
+	init_val=new_first_value;
+}
+
+void generator_define_step(int new_step)
+{
+	step=new_step;
+}
+
+void generator_go_to_beginning(void)
+{
+	current_val=init_val;
+	}
+int generator_get_next(void)
+{
+	int n=current_val;
+	current_val+=step;
+	return n;
+}	
+
+
+
+
+//a voir l'encapsulation 
+//un variable global qui est precedé par mot static ca reste toujours global mais 
+//on private la propreté que la variable est globale c'est de l'encapsulation 
